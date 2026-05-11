6e1f0d23f354d6fddac16559b9b3b5a18297ff49
Rathinakumar <rathinakumar@xerago.com>
2026-05-11 17:35:28 +0530

    config update

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Temple-website/',
+  base: '/',
 })
